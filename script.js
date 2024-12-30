d50f4c29a7f5ce027fdf7be5fd932b54d7547e7d
rania12b <raniaboughadou2@gmail.com>
2024-12-30 20:18:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Fonction pour défiler en douceur vers la section des produits
+function scrollToProducts() {
+    document.querySelector("#products").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+  
+
